c96b9c2826aeb11bba9ceb6f0543ccf09dea37c7
Jace <salendahj@gmail.com>
2024-01-29 18:30:04 +0800

    feat: add pwa

--- resources/views/layouts/landing-main.blade.php
+++ resources/views/layouts/landing-main.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="theme-color" content="#000">
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>JaceInstrumental</title>
@@ -12,6 +13,17 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{ asset("css/landing.css") }}" rel="stylesheet" />
+        <link  href="{{ asset("manifest.json") }}" rel="manifest" />
+
+        <script>
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register('/js/sw.js').then((registration) => {
+                    console.log('Service Worker registered with scope:', registration.scope);
+                }).catch((error) => {
+                    console.error('Service Worker registration failed:', error);
+                });
+            }
+        </script>
     </head>
     <body>
         <!-- Navigation-->
